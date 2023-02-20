1437e8969e26eee0526b233248cde75661a4709f
blackfugee <Augusthne123@gmail.com>
2023-02-20 07:59:30 -0800

    Deployed website

--- src/App.js
+++ src/App.js
@@ -7,20 +7,36 @@ import Testimonials from "./components/testimonials/Testimonials.js"
 import Contact from "./components/contact/Contact.js"
 import Footer from "./components/footer/Footer.js"
 import Portfolios from "./components/portfolios/Portfolios.js"
+import Loader from "./components/loader/Loader.jsx"
+import { useEffect, useState } from "react"
 function App() {
+  const [loading, setloading] = useState(true)
+  useEffect(()=>{
+    fetch(" https://awehstino.github.io/my_portfolio/").then(response =>{
+      console.log(response)
+       setloading(false)
+    }) 
+  },[])
   return (
-  <>
-      <Header />
-      <Nav/>
-      <About/>
-      <Experience/>
-      <Services/>
-      <Portfolios/>
-      <Testimonials />
-      <Contact />
-      <Footer/>
+    <>
+    {
+        loading ? <Loader/> :
+          <>  
+           <Header />
+           <Nav/>
+           <About/>
+           <Experience/>
+           <Services/>
+           <Portfolios/>
+           <Testimonials />
+           <Contact />
+           <Footer/>          
+          </>
+      }
+    <div/>
     </>
-  );
+  )
+ 
 }
 
 export default App;     
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,22 @@
+import "./loader.css"
+
+const Loader = () => {
+  return (
+    <div id="loader-container">
+           <div className="loading-div"> 
+            <div className="loading-div"> 
+             <div className="loading-div">  
+              <div className="loading-div"> 
+               <div className="loading-div">  
+               <div className="loading-div">  
+               </div>
+               </div>
+              </div>
+            </div>
+           </div>
+         </div>
+       </div>
+  )
+}
+
+export default Loader
